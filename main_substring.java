fdbb4d4bdf5898c03f75f5ba02aa68ef4b1a4d95
ethanctrooer <32248618+ethanctrooer@users.noreply.github.com>
2019-09-22 14:44:09 -0700

    Update main_substring.java

--- main_substring.java
+++ main_substring.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * It fulfills the spec by successfully accomplishing these two tasks with user input.
  * There are no current errors; any errors discovered will be corrected and a new version resubmitted.
  * This code has two helper methods, each for one of the tasks. All user input is contained within main.
- * There were no challening parts of this task; I used the internet to look up documentation on some of the used java methods.
+ * There were no challenging parts of this task; I used the internet to look up documentation on some of the used java methods.
  */
 
 /*USABLE TESTCASES---------------------
@@ -53,7 +53,10 @@ public class main_substring {
 		*/
 	}
 	
-	
+	/**
+	 * @param a, sentence with ()
+	 * @return stuff in ()
+	 */
 	public static String extractFirstParen(String a){
 		int par1=0;
 		int par2=1;
@@ -86,6 +89,13 @@ public class main_substring {
 	
 	//---------------------------------------------------------------------------------------------
 	// return a string equal to a, *except* that every occurrence of b in a is substituted for by c.
+	/**
+	 * 
+	 * @param a - master sentence
+	 * @param b - string to be replaced
+	 * @param c - string to replace with
+	 * @return - a with all instances of b replaced with c
+	 */
 	public static String substituteAll(String a, String b, String c){
 		if(b.length()>a.length()) {
 			//return("You done fucked up, girl.");
